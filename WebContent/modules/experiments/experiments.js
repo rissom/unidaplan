67e8e76e76221a80adcab8d8a2269a4dcae24060
rissom <trissom@web.de>
2015-07-07 18:01:47 +0200

    experimente neu gemacht

--- WebContent/modules/experiments/experiments.js
+++ WebContent/modules/experiments/experiments.js
@@ -1,35 +1,52 @@
 (function(){
 'use strict';
-angular.module('unidaplan').controller('expcontroller',['$scope','restfactory',function($scope,restfactory){
-	
-	this.experiments =  [{"creator":"Thorsten Rissom","name":"Erstes Experiment","id":1},
-	                   {"creator":"Thorsten Rissom","name":"Zweites Experiment","id":2}];			
 
-	$scope.myName='Thorsten Rissom';
+function experiments(restfactory,lang) {
 	
-	$scope.loadData = function(ID) {
-		var promise = restfactory.GET("experiments.json?ID="+ID);
+	this.experiments =  {experiments:[{"creator":"Thorsten Rissom","trname":"Platzhalter Experiment","id":1},
+	                   {"creator":"Thorsten Rissom","trname":"Platzhalter Experiment","id":2}]};			
+
+	this.myName='Thorsten Rissom';
+	
+	this.loadData = function() {
+		var promise = restfactory.GET("experiments.json");
+		var expsCtrl=this;
 	    promise.then(function(rest) {
-	    	$scope.experiments = rest.data;
+	    	expsCtrl.experiments = rest.data;
+	    	expsCtrl.translate();
 	    }, function(rest) {
 	    	console.log("ERROR");
 	    });
 	};
 	
+	
 	this.number_of_own_experiments = function() {		
-		var i = this.experiments.length;
+		var i = this.experiments.experiments.length;
 		var x = 0;
 	    while (i--) {
-	       if (this.experiments[i].creator == $scope.myName) {
+	       if (this.experiments.experiments[i].creator == this.myName) {
 		       x++;
 		   }
 	    }
 	    return x;
+//		return 2;
 	};
 	
 	
-    $scope.articleClicked = function(article) {
-    	console.log("articleClicked(article):",article);
-    };
-}]);
+	this.translate = function() {
+		var strings=this.experiments.strings;
+		var exps=this.experiments.experiments;
+		angular.forEach(exps, function(anExp) {
+			angular.forEach(strings, function(translation) {
+				if (anExp.name==translation.string_key && translation.language==lang)
+					{anExp.trname=translation.value;}
+			})
+		})	
+	}
+
+};
+    
+        
+angular.module('unidaplan').controller('expcontroller',['restfactory','lang',experiments]);
+
 })();
--- src/unidaplan/Experiments.java
+++ src/unidaplan/Experiments.java
@@ -1,6 +1,9 @@
 	package unidaplan;
 	import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +11,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 	public class Experiments extends HttpServlet {
 		private static final long serialVersionUID = 1L;
@@ -16,25 +21,48 @@ import org.json.JSONArray;
 	@Override
 	  public void doGet(HttpServletRequest request, HttpServletResponse response)
 	      throws ServletException, IOException {
-	    response.setContentType("text/html");
+		PreparedStatement pstmt;
+		ArrayList<String> stringkeys = new ArrayList<String>(); 
+	    response.setContentType("application/json");
+	    request.setCharacterEncoding("utf-8");
+	    response.setCharacterEncoding("utf-8");
 	    PrintWriter out = response.getWriter();
 	 	DBconnection DBconn=new DBconnection();
 	    DBconn.startDB();
-		String plang="de"; // primary language = Deutsch
-		String slang="en"; // secondary language = english
-		
-	    String query = 
-		"SELECT exp_plan.ID AS ID,users.name as creator, COALESCE (a.value,b.value) as name "
-	    + "FROM  exp_plan \n"
-	    + "JOIN users ON (users.id=exp_plan.Creator) \n"
-	    + "JOIN stringtable a ON (exp_plan.Name=a.string_key AND a.language='"+plang+"')"
-	    + "JOIN stringtable b ON (exp_plan.Name=b.string_key AND b.language='"+slang+"')";
-	    try {  // get json from the database using the query
-			result=DBconn.jsonfromquery(query);
-		} catch (Exception e1) {
-			e1.printStackTrace();
-		}
-		out.println(result.toString());
-		DBconn.closeDB();
-	}
-}	
+	    JSONObject expPlans = new JSONObject();
+	    try {  
+			pstmt= DBconn.conn.prepareStatement( 	
+			"SELECT exp_plan.ID AS ID,users.name as creator, exp_plan.name "
+		    + "FROM  exp_plan \n"
+		    + "JOIN users ON (users.id=exp_plan.Creator)");
+			JSONArray experiments=DBconn.jsonArrayFromPreparedStmt(pstmt);
+			pstmt.close();
+			  for (int i=0; i<experiments.length();i++) {
+	      		  JSONObject tempObj=(JSONObject) experiments.get(i);
+	      		  stringkeys.add(Integer.toString(tempObj.getInt("name")));
+	      	  }
+			  
+			// get the strings
+	        String query="SELECT id,string_key,language,value FROM Stringtable WHERE string_key=ANY('{";
+	      	
+	        StringBuilder buff = new StringBuilder(); // join numbers with commas
+	        String sep = "";
+	        for (String str : stringkeys) {
+         	    buff.append(sep);
+         	    buff.append(str);
+         	    sep = ",";
+	        }
+	        query+= buff.toString() + "}'::int[])";
+	        JSONArray theStrings=DBconn.jsonfromquery(query);
+	        expPlans.put("experiments", experiments);
+	        expPlans.put("strings", theStrings);
+			out.println(expPlans.toString());
+			DBconn.closeDB();
+    	} catch (SQLException e) {
+    		System.err.println("Experiments: Problems with SQL query for Stringkeys");
+    	} catch (JSONException e) {
+			System.err.println("Experiments: JSON Problem while getting Stringkeys");
+    	} catch (Exception e2) {
+			System.err.println("Experiments: Strange Problem while getting Stringkeys");
+    	}
+	}}	
