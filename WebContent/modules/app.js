44e8e7810ed0ab9395b732ffbf9a48cff95ed114
rissom <trissom@web.de>
2015-08-24 15:07:15 +0200

    translations now in extra files

--- WebContent/modules/app.js
+++ WebContent/modules/app.js
@@ -7,7 +7,7 @@ angular.module('unidaplan',['pascalprecht.translate','ui.bootstrap','ui.router']
 
 .config(function($stateProvider, $urlRouterProvider) {
     
-    $urlRouterProvider.otherwise('/login');
+	$urlRouterProvider.otherwise('/login');
     
     $stateProvider
         
@@ -216,124 +216,13 @@ angular.module('unidaplan',['pascalprecht.translate','ui.bootstrap','ui.router']
     
 .config(['$translateProvider', function ($translateProvider) {
   $translateProvider.useSanitizeValueStrategy('escape');
-  $translateProvider.translations('en', {
-  	'Params' : 'Parameters',
-  	'abouttext' : 'Software to organize your experiments',
-	'HELPTEXT' : 'Unidaplan is web-based software for the administration of scientific samples.'			
-  });
- 
-  $translateProvider.translations('de', {
-	'1': 'teststring 1',
-	'About Unidaplan': 'Über Unidaplan',	
-    'Add File': 'Datei hinzufügen',
-    'administration': 'Administration',	
-    'My Experiments': 'Meine Experimente',
-    'Other Experiments': 'Andere Experimente',
-    'Base Parameters': 'Basisparameter',
-    'Cancel' : 'Abbrechen',
-    'chosen samples' : 'ausgewählte Proben',
-    'Create Process' : 'Prozess erstellen',
-    'Divide sample in several new samples' : 'Probe in neue Proben aufteilen',
-    'Assign to process' : 'Mit Prozess assozieren',
-    'created by' : 'erstellt von',
-    'Create Sample' : 'Probe erstellen',
-    'Enter your e-mail here' : 'Bitte E-Mail-Adresse hier eingeben',
-    'Enter full name here' : 'Bitte vollen Namen hier eingeben',
-    'Files': 'Dateien',
-    'Finished Processes' : 'Durchgeführte Prozesse',
-    'Go to process' : 'Zum Prozess gehen',
-    'Mon' : 'Mo',
-    'Tue' : 'Di',
-    'Wed' : 'Mi',
-    'Thu' : 'Do',
-    'Fri' : 'Fr',
-    'Sat' : 'Sa',
-    'Sun' : 'So',
-    'Sample Tree': 'Probenbaum',
-    'Planned Processes': 'Planung',
-    'delete sample': 'Probe löschen',
-    'Experiment': 'Experiment',
-    'No.' : 'Nr.',
-    'Help' : 'Hilfe',
-    'Sample' : 'Probe',
-    'Samples' : 'Proben',
-    'Process' : 'Prozess',
-    'Processes' : 'Prozesse',
-    'next': 'nächste',
-  	'previous' : 'vorherige',
-  	'Is ancestor of' : 'Vorfahr von',
-  	'Originates from' : 'Stammt ab von',
-  	'Delete Sample' : 'Probe löschen',
-  	'Delete Experiment' : 'Experiment löschen',
-  	'Enter your password here' : 'Bitte Passwort hier eingeben',
-  	'Enter your username here' : 'Bitte Benutzernamen hier eingeben',
-  	'Enter processnumber here' : 'Bitte Prozessnumber hier eingeben',
-  	'New Process' : 'Neuer Prozess',
-  	'no.' : 'Nr.',  
-	'day' : 'Tag',
-	'days' : 'Tage',
-	'hour' : 'Stunde',
-	'hours' : 'Stunden',
-  	'Help page' : 'Hilfe Seite',
-  	'Log in' : 'Anmeldung',
-  	'Login' : 'Anmelden',
-  	'New Sample': 'Neue Probe',
-  	'Select processtype' : 'Prozesstyp wählen',
-  	'Password' : 'Passwort',
-  	'Re-enter Password' : 'Passwort wiederholen',
-  	'Re-enter your password here' : 'Bitte Passwort hier wiederholen',
-  	'Recent Samples' : 'zuvor geöffnete Proben',
-  	'Recent Processes' : 'Zuvor geöffnete Prozesse',
-  	'Select sampletype' : 'Wähle Probentyp',
-  	'set role' : 'Rolle setzen',
-  	'Show Process' : 'Zeige Prozess',
-  	'Show Sample' : 'Zeige Probe',
-  	'Show Samples' : 'Zeige Proben',
-  	'Sign Up' : 'Registrieren',
-  	'New Experiment' : 'Neues Experiment',
-  	'Open Experiments' : 'Experimente öffnen',
-  	'Groups and Users' : 'Benutzer und Gruppen',
-  	'Parameters' : 'Parameter',
-  	'Experiments' : 'Experimente',
-  	'Recently viewed Samples' : 'Zuletzt angesehene Proben',
-  	'Recent Experiments' : 'Zuletzt angesehene Experimente',
-  	'Recently viewed Experiments' : 'Zuletzt angesehene Experimente',
-  	'Samples in process' : 'Proben im Prozess',
-  	'Add/Remove samples' : 'Proben hinzufügen/entfernen',
-  	'Delete Process' : 'Prozess löschen',
-  	'Status of process run' : 'Verlauf des Prozesses',
-  	'Processes' : 'Prozesse',
-  	'Logout' : 'Abmelden',
-  	'Params' : 'Parameter',
-  	'and' : 'und',
-  	'Made with' : 'programmiert mit',
-  	'abouttext' : 'Software für die Verwaltung wissenschaftlicher Proben',
-  	'Recently viewed Processes' : 'Zuletzt aufgerufene Prozesse',
-  	'see license' : 'Lizenz',
-  	'as well as' : 'So wie',
-  	'HELPTEXT' : 'Unidaplan ist eine Webbasierte Software um wissenschaftliche Proben zu verwalten. Es können Probentypen'+
-  		  '(z.B. Pulverproben, Solarzellen, Halbleiterlaser, usw.) definiert werden. Die Proben können bestimmte'+
-  		  'Parameter und Eigenschaften aufweisen. Die Auswahl und Definition der Parameter erfolgt über den'+
-  		  'Adminstrator.',
-    'Available Samples':'Verfügbare Proben',
-    'Users' : 'Benutzer',
-    'Username' : 'Benutzername',
-    'Delete User' : 'Lösche Benutzer',
-    'role' : 'Rolle',
-    'E-mail' : 'E-Mail',
-    'Groups' : 'Gruppen',
-    'Delete Group' : 'Gruppe löschen',
-    'Members' : 'Mitglieder',
-    'Search for Sample' : 'Suche nach Probe',
-    'User' : 'Benutzer',
-    'last Login' : 'letzes Mal eingeloggt',
-    'Enter samplename here' : 'Probennamen hier eingeben',
-    'Available sample types' : 'Mögliche Probentypen',
-    'Add User' : 'Benutzer hinzufügen'});
- 
+  $translateProvider.useStaticFilesLoader({
+      prefix: 'languages/lang-',
+      suffix: '.json'
+   });
   $translateProvider.preferredLanguage('en');
 }])
-    
+
 .config(function() {
 	// Put configuration code here
 })
