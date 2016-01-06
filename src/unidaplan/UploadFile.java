ba92236755700b63463da6141ea20118ae397d0f
rissom <trissom@web.de>
2016-01-06 14:47:49 +0100

    Start to implement saving files.

---
+++ src/unidaplan/UploadFile.java
@@ -0,0 +1,120 @@
+package unidaplan;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+@MultipartConfig
+
+	public class UploadFile extends HttpServlet {
+		private static final long serialVersionUID = 1L;
+
+	@Override
+	  public void doPost(HttpServletRequest request, HttpServletResponse response)
+	      throws ServletException, IOException {		
+		Authentificator authentificator = new Authentificator();
+		int userID=authentificator.GetUserID(request,response);
+		int id = -1;
+		String status="ok";
+	    request.setCharacterEncoding("utf-8");
+		response.setContentType("application/json");
+	    response.setCharacterEncoding("utf-8");
+	    
+	    
+	    UploadS3.uploadFileToS3();
+//	    // path to upload directory
+//	    final String path = "uploads";
+//	    
+//	    
+//	    // check if an upload folder exists, if not: create one.
+//	    File theDir = new File(path);
+//
+//		 // if the directory does not exist, create it
+//		 if (!theDir.exists()) {
+//		     System.out.println("creating upload directory");
+//		     boolean result = false;
+//	
+//		     try{
+//		         theDir.mkdir();
+//		         result = true;
+//		     } 
+//		     catch(SecurityException se){
+//		         System.out.println("Error creating Directory!");
+//		         se.printStackTrace();
+//		     }        
+//		     if(result) {   
+////		         System.out.println("DIR created");  
+//		     }
+//		 }
+//	    
+//	    // Create path components to save the file
+//	    final Part filePart = request.getPart("file");
+//	    final String fileName = getFileName(filePart);
+//
+//	    OutputStream out = null;
+//	    InputStream filecontent = null;
+//	    final PrintWriter writer = response.getWriter();
+//
+//	    try {
+//	    	File dings=new File(path + File.separator + fileName);
+//	        out = new FileOutputStream(dings);
+//	        filecontent = filePart.getInputStream();
+//
+//	        int read = 0;
+//	        final byte[] bytes = new byte[1024];
+//
+//	        while ((read = filecontent.read(bytes)) != -1) {
+//	            out.write(bytes, 0, read);
+//	        }
+//	        writer.println("New file " + fileName + " created at " + path);
+////	        LOGGER.log(Level.INFO, "File{0}being uploaded to {1}", 
+////	                new Object[]{fileName, path});
+//	    } catch (FileNotFoundException fne) {
+//	        writer.println("You either did not specify a file to upload or are "
+//	                + "trying to upload a file to a protected or nonexistent "
+//	                + "location.");
+//	        writer.println("<br/> ERROR: " + fne.getMessage());
+//
+////	        LOGGER.log(Level.SEVERE, "Problems during file upload. Error: {0}", 
+////	                new Object[]{fne.getMessage()});
+//	    } finally {
+//	        if (out != null) {
+//	            out.close();
+//	        }
+//	        if (filecontent != null) {
+//	            filecontent.close();
+//	        }
+//	        if (writer != null) {
+//	            writer.close();
+//	        }
+//	    }
+//	}	
+//	
+//	private String getFileName(final Part part) {
+//	    final String partHeader = part.getHeader("content-disposition");
+////	    LOGGER.log(Level.INFO, "Part Header = {0}", partHeader);
+//	    for (String content : part.getHeader("content-disposition").split(";")) {
+//	        if (content.trim().startsWith("filename")) {
+//	            return content.substring(
+//	                    content.indexOf('=') + 1).trim().replace("\"", "");
+//	        }
+//	    }
+//	    return null;
+	}
+}
