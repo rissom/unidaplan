c826807cd31c3d2ce22f110973691a6ab87dde63
rissom <trissom@web.de>
2016-10-18 22:58:07 +0100

    nocachefilter is not used

---
+++ src/filter/NoCacheFilter.java
@@ -0,0 +1,40 @@
+package filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+
+//atWebFilter("*")
+public class NoCacheFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig config) throws ServletException {
+        // If you have any <init-param> in web.xml, then you could get them
+        // here by config.getInitParameter("name") and assign it as field.
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+
+//        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+//        response.setHeader("Pragma", "no-cache"); // HTTP 1.0.
+//        response.setDateHeader("Expires", 0); // Proxies.
+                
+        chain.doFilter(req, res);
+    }
+
+    @Override
+    public void destroy() {
+        // If you have assigned any expensive resources as field of
+        // this Filter class, then you could clean/close them here.
+    }
+
+}
