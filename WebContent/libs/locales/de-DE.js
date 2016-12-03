517103b371d7a180b3ae26de052253ff2cbfc07a
rissom <trissom@web.de>
2016-12-03 20:52:04 +0000

    replaced locales for Excel-Style formatter

--- WebContent/libs/locales/de-DE.js
+++ WebContent/libs/locales/de-DE.js
@@ -1,26 +1,12 @@
-export default {
-  months: ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
-  monthsShort: ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez'],
-  days: ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'],
-  daysShort: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
-  thousandSeparator: ' ',
-  decimalSeparator: ',',
-  formats: {
-    'General Number': '#,#',
-    'Currency': '€ #.##0,00;[Red]€ -#.##0,00',
-    'Fixed': '0,00',
-    'Standard': '#,##0,00',
-    'Percent': '0,00%',
-    'Scientific': '0,00E+00',
-    'Yes/No': '"Ja";"Ja";"Nein"',
-    'True/False': '"Wahr";"Wahr";"Falsch"',
-    'On/Off': '"An";"An";"Aus"',
-    'Short Date': 'dd.mm.yyyy',
-    'Long Date': 'dd mmmm yyyy',
-    'General Date': 'dd.mm.yyyy h:mm',
-    'Medium Date': 'dd.mmm.yy',
-    'Long Time': 'hh:mm:ss',
-    'Short Time': 'h:mm',
-    'Medium Time': 'hh:mm'
-  }
-};
+!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t():
+	"function"==typeof define&&define.amd?define("DataFormatter_enUS",[],t):"object"==typeof
+	exports?exports.DataFormatter_deDE=t():e.DataFormatter_deDE=t()}(
+			this,function(){return function(e){function t(a){if(r[a])return r[a].exports;
+			var o=r[a]={exports:{},id:a,loaded:!1};
+			return e[a].call(o.exports,o,o.exports,t),o.loaded=!0,o.exports}
+			var r={};return t.m=e,t.c=r,t.p="",t(0)}(
+					[function(e,t)
+			           {"use strict";
+			           e.exports={name:"de-DE",
+			           months:['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],monthsShort:['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez'],days:["sunday","monday","tuesday","wednesday","thursday","friday","saturday"],daysShort:['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],thousandSeparator:" ",decimalSeparator:".",formats:{"General Number":"#.#",Currency:"$ #,##0.00;[Red]$ -#,##0.00",Fixed:"0.00",Standard:"#,##0.00",Percent:"0.00%",Scientific:"0.00E+00","Yes/No":'"Yes";"Yes";"No"',"True/False":'"True";"True";"False"',"On/Off":'"On";"On";"Off"',"Short Date":"mm.dd.yyyy","Long Date":"dd mmmm yyyy","General Date":"mm.dd.yyyy h:mm","Medium Date":"dd.mmm.yy",
+	"Long Time":"HH:mm:ss","Short Time":"H:mm","Medium Time":"HH:mm"}}}])});
